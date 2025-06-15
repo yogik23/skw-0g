816d8a1a8bc889080ca098d99939c52d0d172b68
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-15 20:32:39 +0700

    Update logger.js

--- skw/logger.js
+++ skw/logger.js
@@ -5,3 +5,5 @@ export const logCache = (msg) => console.log(chalk.hex("#FF8C00")(`🟡 ${msg}`)
 export const logInfo = (msg) => console.log(chalk.hex("#48D1CC")(`🔵 ${msg}`)); 
 export const logSuccess = (msg) => console.log(chalk.hex("#00FF00")(`🟢 ${msg}`)); 
 export const logError = (msg) => console.log(chalk.hex("#FF6347")(`🔴 ${msg}`)); 
+export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
