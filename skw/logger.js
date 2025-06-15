32dc2164f286aca030a9bde96a8a56ada6040b36
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-15 20:27:43 +0700

    Create logger.js

---
+++ skw/logger.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+export const logAccount = (msg) => console.log(chalk.hex("#A259FF")(`🟣 ${msg}`)); 
+export const logCache = (msg) => console.log(chalk.hex("#FF8C00")(`🟡 ${msg}`)); 
+export const logInfo = (msg) => console.log(chalk.hex("#48D1CC")(`🔵 ${msg}`)); 
+export const logSuccess = (msg) => console.log(chalk.hex("#00FF00")(`🟢 ${msg}`)); 
+export const logError = (msg) => console.log(chalk.hex("#FF6347")(`🔴 ${msg}`)); 
