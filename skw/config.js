e78c2c94d2c23065af58fd73506159e9d12d4931
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-06-18 22:35:12 +0700

    Update config.js

--- skw/config.js
+++ skw/config.js
@@ -8,62 +8,174 @@ import {
   delay,
 } from "./logger.js";
 
-export const ca_swap = "0xE71Af6EEd736bA513CbD19Ebc82D1Cf943b80b63";
-export const ca_routerv3 = "0x86fBDc47994966e14d8BA13f9ED546189641dF71";
-export const ca_approve = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
-export const usdc_address = "0x1AfB500AFfBBc8a7FC8aB0f5C4D06c59AC87B111";
-export const usdt_address = "0x87054392461F52a513d83EF2e06af50f4e2F6614";
-export const wHAUST_address = "0x6C25C1Cb4b8677982791328471be1bFB187687c1";
+export const BTC_ADDRESS = "0x36f6414FF1df609214dDAbA71c84f18bcf00F67d";
+export const ETH_ADDRESS = "0x0fE9B43625fA7EdD663aDcEC0728DD635e4AbF7c";
+export const USDT_ADDRESS = "0x3eC8A8705bE1D5ca90066b37ba62c4183B024ebf";
+export const SWAP_ROUTER = "0xb95B5953FF8ee5D5d9818CdbEfE363ff2191318c";
+export const ca_onChainGM = "0x84A2dc4fd3EFBbAcCc2f2edfC65F1067545275c8";
+export const data_onChainGM = "0x84a3bb6b0000000000000000000000000000000000000000000000000000000000000000";
+export const gm_topic = "0x9290e8f5ba7fa69269d601e86762855088f9a24d834db4d6b3e603d7a522e56a";
+export const GAS_LIMIT = 200000;
 
-export const abi_swap =["function execute(bytes commands, bytes[] inputs, uint256 deadline) payable"];
+export const tokenNames = {
+  [USDT_ADDRESS.toLowerCase()]: "USDT",
+  [ETH_ADDRESS.toLowerCase()]: "ETH",
+  [BTC_ADDRESS.toLowerCase()]: "BTC",
+};
 
-export const erc20Abi = [
-  "function approve(address guy, uint256 wad) public returns (bool)",
-  "function allowance(address owner, address spender) view returns (uint256)"
+export function getTokenName(address) {
+  return tokenNames[address.toLowerCase()] ?? address;
+}
+
+export const erc20_abi = [
+  "function approve(address spender, uint256 amount) external returns (bool)", 
+  "function allowance(address owner, address spender) view returns (uint256)", 
+  "function balanceOf(address owner) view returns (uint256)", 
 ];
 
-export export function randomAmount(min, max, decimalPlaces) {
+export const swap_abi = [
+  "function exactInput((bytes path, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum)) payable returns (uint256 amountOut)",  
+  "function exactInputSingle((address tokenIn, address tokenOut, uint24 fee, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum, uint160 sqrtPriceLimitX96 )) payable returns (uint256)",  
+]
+
+export function RandomAmount(min, max, decimalPlaces) {
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
 }
 
-export export function randomdelay(min = 7000, max = 15000) {
+export function randomdelay(min = 7000, max = 15000) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-export async function approve1(wallet, amount) {
-  const token = new ethers.Contract(wHAUST_address, erc20Abi, wallet);
-  const parsedAmount = ethers.parseUnits(amount, 18);
+export function buildPath({ tokenIn, fee, tokenOut }) {
+  return ethers.concat([
+    tokenIn,
+    ethers.zeroPadValue(ethers.toBeHex(fee), 3),
+    tokenOut,
+  ]);
+}
 
-  const allowance = await token.allowance(wallet.address, ca_approve);
-  if (allowance >= parsedAmount) {
-    return;
-  }
-
-  logCache(`Approving tokens...`);
-
+export async function cekbalance(wallet, tokenIn) {
   try {
-    const tx = await token.approve(ca_approve, ethers.MaxUint256);
-    logInfo(`Tx Dikirim https://explorer-testnet.haust.app/tx/${tx.hash}`);
-    await tx.wait();
-    logSuccess(`Approve successful\n`);
+    const contract = new ethers.Contract(tokenIn, erc20_abi, wallet);
+    const balancewei = await contract.balanceOf(wallet.address);
+    const formatbalance = ethers.formatUnits(balancewei,18);
+    return formatbalance;
   } catch (err) {
-    logError(`Error during Approve : ${err.message || err}`);
+    logError(`Error Cek Balance : ${err.message || err}\n`);
   }
 }
 
-export async function approve2(wallet, tokenAddress, spenderAddress, amount, decimals, expirationTimestamp) {
-  const approve_abi = ["function approve(address token, address spender, uint160 amount, uint48 expiration) external"];
-  const contract = new ethers.Contract(ca_approve, approve_abi, wallet);
-  const parsedAmount = ethers.parseUnits(amount, decimals);
-
-  logCache(`Mengirim Approve...`);
-
+export async function mintToken(wallet, tokenmint) {
   try {
-    const tx = await contract.approve(tokenAddress, spenderAddress, parsedAmount, expirationTimestamp);
-    logInfo(`Tx Dikirim https://explorer-testnet.haust.app/tx/${tx.hash}`);
-    await tx.wait();
-    logSuccess(`Approve Berhasil`);
+    const mint_abi = ["function mint() public"];
+    const contract = new ethers.Contract(tokenmint, mint_abi, wallet);
+
+    logCache(`Claim Faucet ${getTokenName(tokenmint)}`);
+    const tx = await contract.mint();
+    logInfo(`Tx Claim ->> https://chainscan-galileo.0g.ai/tx/${tx.hash}`);
+
+    let attempts = 0;
+    while (attempts < 5) {
+      try {
+        await tx.wait();
+        break;
+      } catch (err) {
+        if (err.message?.includes("Too many requests")) {
+          attempts++;
+          await delay(2000);
+        } else {
+          throw err;
+        }
+      }
+    }
+
+    if (attempts == 5) {
+      logError(`Gagal claim, rate limit\n`);
+    } else {
+      logSuccess(`Claim berhasil!\n`);
+    }
+
   } catch (err) {
-    logError(`Error during Approve : ${err.message || err}`);
+    if (err.reason && err.reason === "Wait 24 hours") {
+      logError(`Claim Masih cooldown..\n`);
+    } else {
+      logError(`Error mintToken ${err.message || err}\n`);
+    }
   }
+}
+
+export async function onChainGM(wallet) {
+  try {
+    const fee = ethers.parseEther("0.00029");
+    logCache(`GM OnChainGM`);
+    const tx = await wallet.sendTransaction({ 
+      to: ca_onChainGM,
+      value: fee,
+      data: data_onChainGM,
+      gasLimit: GAS_LIMIT
+    });
+
+    logInfo(`GM dikirim ->> https://chainscan-galileo.0g.ai/tx/${tx.hash}`);
+    await tx.wait();
+    logSuccess(`OnChainGM berhasil!\n`);
+  } catch (error) {
+    logError(`Error onChainGM : ${error.message || error}\n`);
+  }
+}
+
+export async function approve(wallet, tokenAddress, spenderAddress, amountIn) {
+  try {
+    const token = new ethers.Contract(tokenAddress, erc20_abi, wallet);
+    const allowance = await token.allowance(wallet.address, spenderAddress);
+    if (allowance >= amountIn) {
+      return;
+    }
+    logInfo(`Approve dahulu sebelum swap`);
+
+    const tx = await token.approve(spenderAddress, amountIn);
+    logInfo(`Tx Approve ->> https://chainscan-galileo.0g.ai/tx/${tx.hash}`);
+    await delay(randomdelay());
+
+    await tx.wait();
+    logSuccess(`Approve sukses!`);
+    logCache(`Lanjut ke Swap`);
+
+  } catch (err) {
+    logError(`Error Approve : ${err.message || err}\n`);
+  }
+}
+
+export function generateSwapParams(wallet) {
+  return [
+    {
+      tokenIn: USDT_ADDRESS,
+      tokenOut: ETH_ADDRESS,
+      fee: 3000,
+      recipient: wallet.address,
+      deadline: Math.floor(Date.now() / 1000) + 60,
+      amountIn: ethers.parseUnits(RandomAmount(50, 500, 0), 18),
+      amountOutMinimum: 0,
+      sqrtPriceLimitX96: 0,
+    },
+    {
+      tokenIn: USDT_ADDRESS,
+      tokenOut: BTC_ADDRESS,
+      fee: 3000,
+      recipient: wallet.address,
+      deadline: Math.floor(Date.now() / 1000) + 60,
+      amountIn: ethers.parseUnits(RandomAmount(50, 500, 0), 18),
+      amountOutMinimum: 0,
+      sqrtPriceLimitX96: 0,
+    },
+    {
+      tokenIn: ETH_ADDRESS,
+      tokenOut: USDT_ADDRESS,
+      fee: 3000,
+      recipient: wallet.address,
+      deadline: Math.floor(Date.now() / 1000) + 60,
+      amountIn: ethers.parseUnits(RandomAmount(0.1, 0.5, 2), 18),
+      amountOutMinimum: 0,
+      sqrtPriceLimitX96: 0,
+    },
+  ]
 }
